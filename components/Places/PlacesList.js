ae807d97f32b3564d639036adae773559588ad3c
juanaguileraht <JAguilera@heat-timer.com>
2023-11-16 14:20:59 -0500

    Lesson 193

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 const PlacesList = ({ places }) => {
 
@@ -30,5 +31,6 @@ const styles = StyleSheet.create({
   },
   fallbackText: {
     fontSize: 24,
+    color: Colors.primary200,
   }
 });

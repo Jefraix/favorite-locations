ef6e1991eceb63135bfe51d9649b69871e5a312d
juanaguileraht <JAguilera@heat-timer.com>
2023-11-16 09:37:02 -0500

    Lesson 189

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,9 @@
+import { FlatList } from "react-native";
+
+const PlacesList = ({ places }) => {
+  return (
+    <FlatList data={places} keyExtractor={(item) => item.id} renderItem={} />
+  );
+};
+
+export default PlacesList;

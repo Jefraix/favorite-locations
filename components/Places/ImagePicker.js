43c8953b4a2846e9d50c2dffb027232538b0fb05
Juan Aguilera <jefraix@gmail.com>
2023-11-16 21:10:42 -0500

    Lesson 201

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
-import { Alert, View, Image, StyleSheet } from "react-native";
-import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
-import { Colors } from '../../constants/colors'
+import { Alert, View, Text, Image, StyleSheet } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { Colors } from "../../constants/colors";
 import OutlinedButton from "../UI/OutlinedButton";
 
 const ImagePicker = () => {
   const [pickedImage, setPickedImage] = useState();
 
-  const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
 
   const verifyPermissions = async () => {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
@@ -17,9 +22,12 @@ const ImagePicker = () => {
     }
 
     if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
-      Alert.alert('Insufficient Permissions!', 'You need to grant camera permissions to use this app.')
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
     }
-  }
+  };
 
   const takeImageHandler = async () => {
     const hasPermission = await verifyPermissions();
@@ -33,38 +41,40 @@ const ImagePicker = () => {
       aspect: [16, 9],
       quality: 0.5,
     });
-    
+
     setPickedImage(image.uri);
-  }
+  };
 
   let imagePreview = <Text>No image taken yet.</Text>;
 
   if (pickedImage) {
-    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <OutlinedButton icon='camera' onPress={takeImageHandler}>Take Image</OutlinedButton>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
-}
+};
 
 export default ImagePicker;
 
 const styles = StyleSheet.create({
   imagePreview: {
-    width: '100%',
+    width: "100%",
     height: 200,
     marginVertical: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
   },
   image: {
-    width: '100%',
-    height: '100%',
-  }
-})
+    width: "100%",
+    height: "100%",
+  },
+});
--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,43 +1,77 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 import OutlinedButton from "../UI/OutlinedButton";
 
 import { Colors } from "../../constants/colors";
 
 const LocationPicker = () => {
-  const getLocationHandler = () => {
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
 
-  }
+  const verifyPermissions = async () => {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
 
-  const pickOnMapHandler = () => {
+      return permissionResponse.granted;
+    }
 
-  }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permissions to use this app."
+      );
+    }
+  };
+
+  const getLocationHandler = async () => {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  };
+
+  const pickOnMapHandler = () => {};
 
   return (
     <View>
       <View style={styles.mapPreview}></View>
       <View style={styles.actions}>
-        <OutlinedButton icon='location' onPress={getLocationHandler}>Locate User</OutlinedButton>
-        <OutlinedButton icon='map' onPress={pickOnMapHandler}>Pick on Map</OutlinedButton>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
       </View>
     </View>
   );
-}
+};
 
 export default LocationPicker;
 
 const styles = StyleSheet.create({
   mapPreview: {
-    width: '100%',
+    width: "100%",
     height: 200,
     marginVertical: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
   },
   actions: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
   },
-})
+});

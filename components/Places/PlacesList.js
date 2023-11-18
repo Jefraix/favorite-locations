1054e2d1e182f85dd3e3c57ed341b1e5df077923
Juan Aguilera <jefraix@gmail.com>
2023-11-18 16:43:50 -0500

    Lesson 212

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -3,20 +3,22 @@ import PlaceItem from "./PlaceItem";
 import { Colors } from "../../constants/colors";
 
 const PlacesList = ({ places }) => {
-
   if (!places || places.length === 0) {
     return (
       <View style={styles.fallbackContainer}>
-        <Text style={styles.fallbackText}>No places added yet - start adding some!</Text>
+        <Text style={styles.fallbackText}>
+          No places added yet - start adding some!
+        </Text>
       </View>
     );
   }
 
   return (
-    <FlatList 
-    data={places} 
-    keyExtractor={(item) => item.id} 
-    renderItem={({item}) => <PlaceItem place={item} />} 
+    <FlatList
+      style={styles.list}
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
     />
   );
 };
@@ -24,14 +26,17 @@ const PlacesList = ({ places }) => {
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   fallbackText: {
     fontSize: 24,
     color: Colors.primary200,
-    textAlign: 'center',
-  }
+    textAlign: "center",
+  },
 });

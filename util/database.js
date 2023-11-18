2d21336b4d22f0be3bf7eb959e3310a3127cf751
Juan Aguilera <jefraix@gmail.com>
2023-11-18 17:09:00 -0500

    Lesson 214

--- util/database.js
+++ util/database.js
@@ -27,3 +27,27 @@ export const init = () => {
 
   return promise;
 };
+
+export const insertPlace = () => {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+        [
+          place.title,
+          place.imageUri,
+          place.address,
+          place.location.lat,
+          place.location.lng,
+        ],
+        (_, result) => {
+          console.log(result);
+          resolve();
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+};
